451dfd81ccd4143533b879dde351158d42315d3a
kevin bhavsar <kevinbhavsar567@gmail.com>
2021-09-03 00:58:20 +0530

    add product screen and routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "../App.css";
 
 const Header = () => {
@@ -7,17 +8,23 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">V-Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>V-Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="pop">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart px-1"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                {" "}
-                <i className="fas fa-user px-1"></i>Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart px-1"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  {" "}
+                  <i className="fas fa-user px-1"></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
